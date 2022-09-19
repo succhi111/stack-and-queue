974952973f67a3c6b3c3e7e96778402acef9545f
succhi111 <suchendramishra396@gmail.com>
2022-09-19 12:16:01 +0530

    insert element in stack

---
+++ src/com/bridgelabz/Node.java
@@ -0,0 +1,6 @@
+package com.bridgelabz;
+
+public class Node<T> {
+    T data;
+    Node<T> next;
+}
---
+++ src/com/bridgelabz/Stack.java
@@ -0,0 +1,13 @@
+package com.bridgelabz;
+
+public class Stack <T>{
+    LinkedList<T> linkedList = new LinkedList<>();
+    public void push(T data) {
+        linkedList.addAtStart(data);
+    }
+
+
+    public void print(){
+        linkedList.print();
+    }
+}
